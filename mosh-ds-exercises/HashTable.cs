f5161a55a05291e7b1128a1b1ef46cc303f26b9a
Gilbert R <gilbert@megsoftconsulting.com>
2023-02-14 09:43:57 -0400

    fix hash table implementation

--- mosh-ds-exercises/HashTable.cs
+++ mosh-ds-exercises/HashTable.cs
@@ -4,8 +4,8 @@ public class HashTable
 {
     private class Entry
     {
-        public readonly int Key;
-        public readonly string Value;
+        public int Key;
+        public string Value;
 
         public Entry(int key, string value)
         {
@@ -24,50 +24,85 @@ public class HashTable
 
     private int GetHashedKey(int key)
     {
-        return key % _size;
+        return Math.Abs(key) % _size;
     }
 
     public void Put(int key, string value)
     {
+        // []
+        // [[{key: 3, value: 'Peter'}], [{key: 4, value: 'Flor'}, {key: 1, value: 'John'} ], [{key: 2, value: 'Dean'}]]
+
         var hashedKey = GetHashedKey(key);
         var entry = new Entry(key, value);
         if (_table[hashedKey] == null)
         {
             var list = new LinkedList<Entry>();
-            list.AddLast(entry);
+            list.AddFirst(entry);
             _table[hashedKey] = list;
         }
         else
         {
-            _table[hashedKey].AddLast(entry);
+            var curr = _table[hashedKey].First;
+            var found = false;
+            while (curr != null)
+            {
+                found = curr.Value.Key == key;
+                if (found)
+                {
+                    curr.Value.Value = value;
+                    break;
+                }
+                curr = curr.Next;
+            }
+            
+            if (!found) _table[hashedKey].AddFirst(entry);
         }
     }
 
     public string Get(int key)
     {
-        if (!ContainsKey(key)) throw new Exception($"{key} not found");
+        string result = null;
+        if (!ContainsKey(key)) return result;
 
         var hashedKey = GetHashedKey(key);
         var curr = _table[hashedKey].First;
         
         while (curr != null)
         {
-            if (curr.Value.Key == key) return curr.Value.Value;
+            if (curr.Value.Key == key) result = curr.Value.Value;
             curr = curr.Next;
         }
-        return null;
+        return result;
     }
 
-    public void Remove(int key)
+    public void Remove(int key) // key = 4 || 1 -> 1
     {
+        // [[{key: 3, value: 'Peter'}], [{key: 4, value: 'Flor'}, {key: 1, value: 'John'} ], [{key: 2, value: 'Dean'}]]
+        //                                                              c
         if (!ContainsKey(key)) throw new Exception($"{key} not found");
+
         var hashedKey = GetHashedKey(key);
-        _table[hashedKey] = null;
+        var curr = _table[hashedKey].First;
+        while (curr != null)
+        {
+            if (curr.Value.Key == key)
+            {
+                _table[hashedKey].Remove(curr);
+                return;
+            }
+            curr = curr.Next;
+        }
     }
 
-    public bool ContainsKey(int key)
+    public bool ContainsKey(int key) // key = 4 || 1 -> 1
     {
         var hashedKey = GetHashedKey(key);
-        return _table[hashedKey] != null;
+        var curr = _table[hashedKey].First;
+        while (curr != null)
+        {
+            if (curr.Value.Key == key) return true;
+            curr = curr.Next;
+        }
+        return false;
     }
 }
--- mosh-ds-exercises/Program.cs
+++ mosh-ds-exercises/Program.cs
@@ -13,12 +13,19 @@ using HashTable = mosh_ds_exercises.HashTable;
 var hashtable = new HashTable(3);
 
 hashtable.Put(1, "John");
-hashtable.Put(3, "Dean");
-hashtable.Put(6, "Peter");
-hashtable.Put(2, "Mary");
-Console.WriteLine(hashtable.ContainsKey(2));
+hashtable.Put(2, "Dean");
+hashtable.Put(3, "Peter");
+hashtable.Put(4, "Mary");
+hashtable.Put(4, "Flor");
 
-var val = hashtable.Get(2);
-hashtable.Remove(2);
+Console.WriteLine(hashtable.ContainsKey(1));
+var key = 1;
+hashtable.Remove(key);
+hashtable.Remove(4);
+hashtable.Put(4, "Luis");
+
+var val = hashtable.Get(4);
+
+
 Console.WriteLine(val);
-Console.WriteLine(hashtable.ContainsKey(2));
+// Console.WriteLine(hashtable.ContainsKey(key));
